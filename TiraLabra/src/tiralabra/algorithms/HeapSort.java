e8550b3c54ecdb187a2abcf2534eab1acf571a20
jajojajo <joel.jarvinen@cs.helsinki.fi>
2014-01-21 20:08:48 +0200

    javadocs

--- TiraLabra/src/tiralabra/algorithms/HeapSort.java
+++ TiraLabra/src/tiralabra/algorithms/HeapSort.java
@@ -8,6 +8,11 @@ public class HeapSort {
 
     private int heapSize;
 
+    /**
+     * Sort an list using heap sort
+     *
+     * @param list list to be sorted
+     */
     public void sort(int[] list) {
         buildHeap(list);
         for (int i = heapSize; i > 0; i--) {
@@ -17,6 +22,11 @@ public class HeapSort {
         }
     }
 
+    /**
+     * Change the array so it becomes a heap-like data structure.
+     *
+     * @param A list to be modified
+     */
     private void buildHeap(int[] A) {
         heapSize = A.length - 1;
         for (int i = heapSize / 2; i >= 0; i--) {
@@ -24,6 +34,19 @@ public class HeapSort {
         }
     }
 
+    /**
+     * Check that the heap conditions are met at the given index of the array.
+     * <p>
+     * If either of the nodes children are larger than the node itself, swap the
+     * larger children and the nodes position. And recursively call heapify at
+     * the larger child's position. If there is only left child which is larger
+     * than the node itself swap them.
+     *
+     *
+     *
+     * @param A Array
+     * @param index index of the node to be inspected
+     */
     private void heapify(int[] A, int index) {
         int left = leftChild(index);
         int right = rightChild(index);
@@ -44,16 +67,35 @@ public class HeapSort {
         }
     }
 
+    /**
+     * Swap two nodes in the list with each other.
+     *
+     * @param A list
+     * @param i first node to be swapped
+     * @param k second node to be swapped
+     */
     private void swap(int[] A, int i, int k) {
         int temp = A[i];
         A[i] = A[k];
         A[k] = temp;
     }
 
+    /**
+     * Get the index of nodes left child
+     *
+     * @param index nodes index
+     * @return index of the nodes left child
+     */
     private int leftChild(int index) {
         return index * 2;
     }
 
+    /**
+     * Get the index of nodes right child
+     *
+     * @param index nodes index
+     * @return index of the nodes right child
+     */
     private int rightChild(int index) {
         return index * 2 + 1;
     }
